ad17b31800f95da8eced80986e43f70d49f0de44
Nandhu Santhosh <nandhusanthosh87@gmail.com>
2023-06-04 14:36:36 +0530

    the header and the second section which is a carousel is donw and is responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+let searchBtn = document.getElementById('searchBtn')
+let accountBtn = document.getElementById('accountBtn')
+let cartButton = document.getElementById('cartBtn')
+
+searchBtn.addEventListener('click', showSearchBar);
+accountBtn.addEventListener('click', showLogoutSection);
+cartButton.addEventListener('click', showCart);
+
+
+
+// to implement
+function showSearchBar(e){console.log(e)}
+function showLogoutSection(e){console.log(e)}
+function showCart(e){console.log(e)}
