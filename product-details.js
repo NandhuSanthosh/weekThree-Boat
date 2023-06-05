cc263960278a44cb935a2963d5de853adc03336d
Nandhu Santhosh <nandhusanthosh87@gmail.com>
2023-06-05 13:36:37 +0530

    product details are rendered dynamically, and also added some more sections

--- product-details.js
+++ product-details.js
@@ -1,5 +1,5 @@
 class ProductDetails{
-    constructor(name, ogPrice, currPrice, rating, colourArray, usb, attraction){
+    constructor(name, ogPrice, currPrice, rating, colourArray, usb, attraction, imgLink){
         this.id = Date.now();
         this.name = name;
         this.ogPrice = ogPrice;
@@ -8,8 +8,29 @@ class ProductDetails{
         this.colorVariants = colourArray;
         this.usb = usb;
         this.attraction = attraction;
+        this.imgLink = imgLink;
 
     }
 }
 
+const dummyData = [];
+dummyData.push(new ProductDetails("Airdopes 131", 2990, 999, 4.8, ["#000000","#d48f87", "#c5b898", "lightblue", "lightgreen", "blue", "green"],"Super Saver" ,"60 Hours Playback", "./assets/daily-deals/bd-1.jpg"))
+dummyData.push(new ProductDetails("Rockerz 235 V2", 2990, 999, 4.8, [ "#000000", "#6b8e23", "#24249c", "blue"],"Super Saver" ,"8 Hours Playback", "./assets/daily-deals/bd-3.jpg"))
+dummyData.push(new ProductDetails("Wave Lite", 6990, 1799, 4.9, ["#ff0000","#000000", "#001cff", "red", "blue"],"Super Saver" ,"Health Monitoring", "./assets/daily-deals/bd-4.jpg"))
+dummyData.push(new ProductDetails("Storm Call", 1799, 7999, 4.7, ["#000000", "#ff0000", "#24249c"],"Super Saver" ,"BT Calling", "./assets/daily-deals/bd-2.jpg"))
+
+dummyData.push(new ProductDetails("Ultima Cennect Max", 8999, 2199, "Be the first to review", ["#000000", "#1c2238", "#a08e80"],"New Launch" ,"BT Calling", "./assets/new-launches/p1.jpg"))
+dummyData.push(new ProductDetails("Ultima Call Max", 8499, 2499, "Be the first to review", ["#000000", "#939393", "#e2cec0"],"New Launch" ,"BT Calling", "./assets/new-launches/p2.png"))
+dummyData.push(new ProductDetails("Rockerz 255 touch", 1499, 2999, "Be the first to review", ["#000000", "#003eff", "#79afb4"],"New Launch" ,"30 HOURS PLAYBACK", "./assets/new-launches/p3.png"))
+dummyData.push(new ProductDetails("Aavante Bar Chord", 21999, 9499, "Be the first to review", ["#000000", "#003eff", "#79afb4"],"New Launch" ,"Wireless Subwoofer", "./assets/new-launches/p4.png"))
+
+dummyData.push(new ProductDetails("Immortal 121", 3499, 1699, "4.8", ["#000000", "#d7d7d7"],"" ,"40 Hours Playback", "./assets/best-of-boat/b1.gif"))
+dummyData.push(new ProductDetails("Airdopes 115", 2990, 1499, "4.9", ["#d7d7d7", "#003EFF", "#DC3545"],"Super Saver" ,"Clear Callingg", "./assets/best-of-boat/b2.jpg"))
+dummyData.push(new ProductDetails("Airdopes 131 PRO", 2990, 1299, "4.9", ["#393939", "#b77a8d", "#003EFF"],"" ,"45 Hours Playback", "./assets/best-of-boat/b3.webp"))
+dummyData.push(new ProductDetails("Airdopes 161", 2490, 1099, "4.9", ["#657286","#d7d7d7", "#000000"],"" ,"17 Hours Playback", "./assets/best-of-boat/b4.jpg"))
+
+
+const dailyDeals = ["Airdopes 131","Storm Call", "Rockerz 235 V2" ,"Wave Lite"]
+const newLaunches = ["Ultima Cennect Max", "Ultima Call Max","Rockerz 255 touch","Aavante Bar Chord"]
+const bestOfBoat = ["Immortal 121","Airdopes 115","Airdopes 131 PRO","Airdopes 161"]
 
--- script.js
+++ script.js
@@ -2,10 +2,164 @@ let searchBtn = document.getElementById('searchBtn')
 let accountBtn = document.getElementById('accountBtn')
 let cartButton = document.getElementById('cartBtn')
 
+
 searchBtn.addEventListener('click', showSearchBar);
 accountBtn.addEventListener('click', showLogoutSection);
 cartButton.addEventListener('click', showCart);
 
+// console.log(dummyData)
+// console.log(dailyDeals)
+
+dailyDealsLoader();
+newLaunchesLoader();
+bestOfBoatLoader();
+
+function dailyDealsLoader(){
+    let dailyDealsContainer = document.getElementById('daily-deals-container')
+    const dailyDealsProduct = findProducts(dummyData, dailyDeals);
+
+    for(var i = 0; i<dailyDealsProduct.length && i<4; i++){
+        const proTile = createProductTile(dailyDealsProduct[i]);
+        dailyDealsContainer.appendChild(proTile);
+    }
+}
+
+function newLaunchesLoader(){
+    const newLaunchesContainer = document.getElementById('new-launches-container')
+    const newLaunchesProducts = findProducts(dummyData, newLaunches);
+
+    for(var i = 0; i<newLaunchesProducts.length && i<4; i++){
+        const proTile = createProductTile(newLaunchesProducts[i]);
+        newLaunchesContainer.appendChild(proTile)
+    }
+}
+
+function bestOfBoatLoader(){
+    const bestOfBoatContainer = document.getElementById('best-of-container')
+    const bestOfBoatProducts = findProducts(dummyData, bestOfBoat);
+
+    for(var i = 0; i<bestOfBoatProducts.length && i<4; i++){
+        const proTile = createProductTile(bestOfBoatProducts[i]);
+        bestOfBoatContainer.appendChild(proTile)
+    }
+}
+
+
+function findProducts(dummyData, catogory) {
+    let output = dummyData.filter( (product) => {
+        for(p of catogory){
+            if(p === product.name){
+                return 1;
+            }
+        }
+        return 0;
+    })
+    return output;
+}
+
+function createProductTile(proDetails){
+
+    const discountPercentage = findPercentage(proDetails.currPrice, proDetails.ogPrice);
+
+    const productTile = document.createElement('div');
+    productTile.classList.add('product-tile');
+
+    const tile = `
+                        <div class="image-container" >
+                            <img src="${proDetails.imgLink}" alt="">
+                            <div class="usb">
+                                <div >
+                                    <div class="d-flex justify-content-center">
+                                        <span class="fs-8 font-weight-bold" >${proDetails.attraction}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-3 ">
+                            <div class="d-flex justify-content-between align-items-center fs-7">
+                                <div>
+                                    <i class="fa-solid fa-star clr-golden"></i>
+                                    <span class='fs-9 font-weight-500'>${proDetails.rating}</span>
+                                </div>
+                    
+                                <div class="d-flex align-items-center color-varient-container">
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-center mt-2">
+                                <div class="product-details fs-7 ">
+                                    <span class="font-weight-bold">${proDetails.name}</span>
+                                    <div class="mt-1">
+                                        <span class=" fs-6 font-weight-bold">₹${proDetails.currPrice}</span>
+                                        <span class="fc-gray-500 text-line-through fs-7 font-weight-400">₹${proDetails.ogPrice}</span>
+                                        <span class="success fs-9 font-weight-bold">${discountPercentage}% off</span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button class="add-to-cart-btn fs-7 font-weight-500">Add To Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                        
+    `
+    
+    
+    productTile.innerHTML = tile;
+    
+    if(proDetails.usb != ""){
+        // console.log(proDetails.attraction)
+        const attration = `
+            <div >
+                <span>
+                    <span><i class="fa-solid fa-bolt fs-8" style="color:rgb(255, 102, 0)"></i></span>
+                    <span class="fs-8">${proDetails.usb}</span>
+                </span>
+            </div>
+        `
+
+        const attrationContainer = document.createElement('div');
+        attrationContainer.classList.add('attraction')
+        attrationContainer.innerHTML = attration;
+        productTile.append(attrationContainer)
+    }
+
+    if(proDetails.colorVariants.length != 0){
+        const colorPelletContainer = productTile.getElementsByClassName('color-varient-container')[0];
+        
+        
+        // creating and appending the color pellets
+        const innerContainer = document.createElement('div');
+        colorPelletContainer.appendChild(innerContainer);
+        innerContainer.classList.add('fs-8', 'font-weight-400', 'align-items-end', 'd-flex')
+
+        let colors = "";
+        for(let i = 0; i<3 && i<proDetails.colorVariants.length; i++){
+            colors += `<span class="coller-pellet" style="background-color: ${proDetails.colorVariants[i]}"></span>`
+        }
+
+        innerContainer.innerHTML = colors;
+
+        // the extra color indicator
+        if(proDetails.colorVariants.length > 3){
+            console.log("what the fuck")
+            const extraColor = document.createElement('div');
+            extraColor.classList.add('ms-1');
+            extraColor.innerHTML = `<span>+${proDetails.colorVariants.length - 3}</span>`
+            innerContainer.appendChild(extraColor)
+        }
+    }
+
+    return productTile;
+
+
+}
+
+
+// to calcualte percentage;
+function findPercentage(currPrice, ogPrice){
+    const onePercentage = ogPrice / 100;
+    const amountDiscount = ogPrice - currPrice;
+    return  Math.ceil(amountDiscount / onePercentage);
+}
 
 
 // to implement
