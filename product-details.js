4c52b42249509e9c9633b503722ccd75df35b7e9
Nandhu Santhosh <nandhusanthosh87@gmail.com>
2023-06-05 19:19:21 +0530

    the blog and some other section are implemented and made it repsonsive, no bug spoted

--- product-details.js
+++ product-details.js
@@ -17,11 +17,11 @@ const dummyData = [];
 dummyData.push(new ProductDetails("Airdopes 131", 2990, 999, 4.8, ["#000000","#d48f87", "#c5b898", "lightblue", "lightgreen", "blue", "green"],"Super Saver" ,"60 Hours Playback", "./assets/daily-deals/bd-1.jpg"))
 dummyData.push(new ProductDetails("Rockerz 235 V2", 2990, 999, 4.8, [ "#000000", "#6b8e23", "#24249c", "blue"],"Super Saver" ,"8 Hours Playback", "./assets/daily-deals/bd-3.jpg"))
 dummyData.push(new ProductDetails("Wave Lite", 6990, 1799, 4.9, ["#ff0000","#000000", "#001cff", "red", "blue"],"Super Saver" ,"Health Monitoring", "./assets/daily-deals/bd-4.jpg"))
-dummyData.push(new ProductDetails("Storm Call", 1799, 7999, 4.7, ["#000000", "#ff0000", "#24249c"],"Super Saver" ,"BT Calling", "./assets/daily-deals/bd-2.jpg"))
+dummyData.push(new ProductDetails("Storm Call",7999 , 1799, 4.7, ["#000000", "#ff0000", "#24249c"],"Super Saver" ,"BT Calling", "./assets/daily-deals/bd-2.jpg"))
 
 dummyData.push(new ProductDetails("Ultima Cennect Max", 8999, 2199, "Be the first to review", ["#000000", "#1c2238", "#a08e80"],"New Launch" ,"BT Calling", "./assets/new-launches/p1.jpg"))
 dummyData.push(new ProductDetails("Ultima Call Max", 8499, 2499, "Be the first to review", ["#000000", "#939393", "#e2cec0"],"New Launch" ,"BT Calling", "./assets/new-launches/p2.png"))
-dummyData.push(new ProductDetails("Rockerz 255 touch", 1499, 2999, "Be the first to review", ["#000000", "#003eff", "#79afb4"],"New Launch" ,"30 HOURS PLAYBACK", "./assets/new-launches/p3.png"))
+dummyData.push(new ProductDetails("Rockerz 255 touch",2999 ,1499, "Be the first to review", ["#000000", "#003eff", "#79afb4"],"New Launch" ,"30 HOURS PLAYBACK", "./assets/new-launches/p3.png"))
 dummyData.push(new ProductDetails("Aavante Bar Chord", 21999, 9499, "Be the first to review", ["#000000", "#003eff", "#79afb4"],"New Launch" ,"Wireless Subwoofer", "./assets/new-launches/p4.png"))
 
 dummyData.push(new ProductDetails("Immortal 121", 3499, 1699, "4.8", ["#000000", "#d7d7d7"],"" ,"40 Hours Playback", "./assets/best-of-boat/b1.gif"))
